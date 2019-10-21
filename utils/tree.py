ed93cbb7b7c56fc2ce6f1a951de5c0158477aff3
jvrobinho <jvmonteirorj@gmail.com>
2019-10-21 14:10:55 -0300

    NEW: tree simple structure

---
+++ utils/tree.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self,type,children=None,leaf=None):
+        self.type = type
+        if children:
+            self.children = list(filter(None, children))
+        else:
+            self.children = [ ]
+        if leaf:
+            self.leaf = list(filter(None, leaf))
+        else:
+            self.leaf = [ ]
+    
+    def __str__(self):
+        return "[NODE]: %s" % (self.type)
+
+    def pretty(self):
+        i = 0
+        print("[NODE]: %s  - %d children - " % (self.type, len(self.children)))
+        print("\t[TOKENS]: %s" % (self.leaf))
+        while i < len(self.children):
+            print("\t[CHILD #%d]: %s" % (i, self.children[i]))
+            self.children[i].pretty()
+            i += 1    
