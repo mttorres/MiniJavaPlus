b9247f23c39f8392acf2f42406948d4f5e592062
Arch <33100644+mttorres@users.noreply.github.com>
2019-10-20 23:58:05 -0300

    Parser montado com a gramatica como BNF padrao, note que ele ainda nao funciona corretamente.

--- src/minijavaPARSER.py
+++ src/minijavaPARSER.py
@@ -1,29 +1,77 @@
 import sys
+
 sys.path.append("../")
 from src import minijavaLEX
 from ply import yacc
 
 tokens = minijavaLEX.tokens
 
-#REGRAS DA GRAMATICA : ESCREVE-SE SEMPRE ASSIM-> producao1 : producao2  TOKEN(mesmo nome definido no LEXER)  , e utilizamos as notações EBNF
+
+# REGRAS DA GRAMATICA : ESCREVE-SE SEMPRE ASSIM-> producao1 : producao2  TOKEN(mesmo nome definido no LEXER)  , e utilizamos as notações EBNF
 # aparentemente ele so aceita BNF.. deve -se converter de EBNF para BNF
+# https://stackoverflow.com/questions/2466484/converting-ebnf-to-bnf (COMO FAZER)
+# note que os nomes dessas produções criadas para transformar em BNF eu dei baseado na minha da gramatica interpretação(pode estar errada)
 def p_prog(p):
-    "prog : main { classe }"
+    # "prog : main { classe }"
+    "prog : main  multiclass"
+
+
+def p_multiclass(p):
+    '''multiclass : multiclass classe
+                  | empty '''
+
 
 def p_main(p):
-    "main : CLASS ID LBRACE PUBLIC STATIC VOID MAIN LPAREN STRING LBRACK RBRACK RPAREN LBRACE cmd RBRACE RBRACE"
+    "main : CLASS ID LBRACE PUBLIC STATIC VOID MAIN LPAREN STRING LBRACK RBRACK  ID RPAREN LBRACE cmd RBRACE RBRACE"
+
 
 def p_classe(p):
-    "classe : CLASS ID [EXTENDS ID] LBRACE {var} {metodo} RBRACE"
+    "classe : CLASS ID extends LBRACE variaveis metodos RBRACE"
+
+
+def p_extends(p):
+    '''extends : EXTENDS ID
+              | empty '''
+
+
+# mais especificamente variaveis opcionais de classe ou metodos
+def p_variaveis(p):
+    '''variaveis : variaveis var
+                 | empty'''
+
+
+def p_metodos(p):
+    '''metodos : metodos metodo
+               | empty'''
+
 
 def p_var(p):
     "var : tipo ID"
 
+
 def p_metodo(p):
-    "metodo : PUBLIC tipo ID LPAREN [params] RPAREN LBRACE {var} {cmd} RETURN exp SEMI RBRACE "
+    "metodo : PUBLIC tipo ID LPAREN paramsopcional RPAREN LBRACE variaveis cmds RETURN exp SEMI RBRACE "
+
+
+def p_paramsopcional(p):
+    '''paramsopcional : params
+                      | empty'''
+
+
+def p_cmds(p):
+    '''cmds : cmds cmd
+            | empty'''
+
 
 def p_params(p):
-    "params : tipo ID {COMMA tipo ID}"
+    '''params : tipo ID listaparamsextra
+              | empty'''
+
+
+def p_listaparamsextra(p):
+    '''listaparamsextra : listaparamsextra COMMA tipo ID
+                        | empty'''
+
 
 def p_tipo(p):
     '''tipo : INT LBRACK RBRACK
@@ -31,8 +79,9 @@ def p_tipo(p):
             | INT
             | ID '''
 
+
 def p_cmd(p):
-   '''cmd : LBRACE {cmd} RBRACE
+    '''cmd : LBRACE cmds RBRACE
           | IF LPAREN exp RPAREN cmd
           | IF LPAREN exp RPAREN cmd ELSE cmd
           | WHILE LPAREN exp RPAREN cmd
@@ -40,10 +89,12 @@ def p_cmd(p):
           | ID ASSIGN exp SEMI
           | ID LBRACK exp RBRACK ASSIGN exp SEMI '''
 
+
 def p_exp(p):
     '''exp : exp LAND rexp
            | exp LOR rexp
            | rexp '''
+
 
 def p_rexp(p):
     '''rexp : rexp LT aexp
@@ -51,15 +102,18 @@ def p_rexp(p):
             | rexp NE aexp
             | aexp'''
 
+
 def p_aexp(p):
     '''aexp : aexp PLUS mexp
             | aexp MINUS mexp
             | mexp'''
 
+
 def p_mexp(p):
     '''mexp : mexp TIMES sexp
             | mexp DIVIDE sexp
             | sexp'''
+
 
 def p_sexp(p):  # MINUS sexp  ?
     '''sexp : LNOT sexp
@@ -73,21 +127,42 @@ def p_sexp(p):  # MINUS sexp  ?
             | pexp LBRACK exp RBRACK
             | pexp '''
 
+
 def p_pexp(p):
     '''pexp : ID
             | THIS
             | NEW ID LPAREN RPAREN
             | pexp POINT ID
-            | pexp POINT ID LPAREN [exps] RPAREN '''
+            | pexp POINT ID LPAREN expopcionalmetodo RPAREN '''
+
+
+def p_expopcionalmetodo(p):
+    '''expopcionalmetodo : exps
+                        | empty'''
+
 
 def p_exps(p):
-    '''exps : exp {COMMA exp}'''
+    '''exps : exp expslist
+            | empty '''
+
+
+def p_expslist(p):
+    '''expslist : expslist COMMA exp
+                | empty '''
+
+
+# define as produções vazias (fica mais facil de visualizar que escrever vazio varias vezes)
+def p_empty(p):
+    '''empty : '''
+
 
 def p_error(p):
     print("Erro sintatico no input!")
 
-parser = yacc.yacc()
 
-def generateTree(line,SYNTAX_TREE):
+parser = yacc.yacc(method='SLR')
+
+
+def generateTree(line, SYNTAX_TREE):
     resultado = parser.parse(line)
     SYNTAX_TREE.append(resultado)
