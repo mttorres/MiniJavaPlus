0737acc9035bdda44f6af0b48ad052bcfc7e289f
jvrobinho <jvmonteirorj@gmail.com>
2019-10-21 15:12:52 -0300

    QoL: counts Node's  tokens when pretty printing

--- utils/tree.py
+++ utils/tree.py
@@ -9,15 +9,15 @@ class Node:
             self.leaf = list(filter(None, leaf))
         else:
             self.leaf = [ ]
-    
+
     def __str__(self):
         return "[NODE]: %s" % (self.type)
 
     def pretty(self):
         i = 0
-        print("[NODE]: %s  - %d children - " % (self.type, len(self.children)))
+        print("[NODE]: %s  - %d children - %d tokens" % (self.type, len(self.children), len(self.leaf)))
         print("\t[TOKENS]: %s" % (self.leaf))
         while i < len(self.children):
-            print("\t[CHILD #%d]: %s" % (i, self.children[i]))
+            print("\t[CHILD #%d]: %s\n" % (i, self.children[i]))
             self.children[i].pretty()
             i += 1    
